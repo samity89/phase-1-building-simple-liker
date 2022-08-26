5875d94403bed1bafe9ada2e27adef9aef606255
Samuel Amity <sam.amity@gmail.com>
2022-08-26 18:03:39 -0400

    Done with assignment

--- main.js
+++ main.js
@@ -1,11 +1,32 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+const likeButton = document.querySelectorAll('span.like-glyph')
+// const errorModal = document.querySelector("#modal")
 
-// Your JavaScript code goes here!
+function likeListener(likeButton) {
+  likeButton.addEventListener("click", (e) => {
+    mimicServerCall()
+    .then(() => {
+      if (e.target.innerText === EMPTY_HEART) {
+        e.target.innerText = FULL_HEART
+        e.target.className = "activated-heart"
+      }
+      else if (e.target.innerText === FULL_HEART) {
+        e.target.innerText = EMPTY_HEART
+        e.target.className = ""
+      }
+    })
+    .catch((error) => {
+      const errorModal = document.querySelector("#modal")    
+      errorModal.className = ""
+      setTimeout(() => errorModal.className = "hidden", 3000);
+      console.log(errorModal)
+    })
+  })
+}
 
-
-
+addEventListener("DOMContentLoaded", likeButton.forEach(likeListener))
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
